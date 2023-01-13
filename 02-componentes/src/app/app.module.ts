7cfe7d231eed20e39e510c35417ebf8611634cca
tati oliveira <tat1roliv@gmail.com>
2023-01-12 22:36:07 -0300

    aula02

--- 02-componentes/src/app/app.module.ts
+++ 02-componentes/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { LoginComponent } from './components/login/login.component';
+import { LoginAdministradorComponent } from './components/login-administrador/login-administrador.component';
+import { LoginEstudianteComponent } from './components/login-estudiante/login-estudiante.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { StudentsComponent } from './components/students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    LoginComponent,
+    LoginAdministradorComponent,
+    LoginEstudianteComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
