c9f8d0b014f2b733c7ab8b611fa7ba0f55797097
tati oliveira <tat1roliv@gmail.com>
2023-01-23 09:18:58 -0300

    directivas personalizadas

--- 04-interpolacion-directivas-/demo/src/app/app.module.ts
+++ 04-interpolacion-directivas-/demo/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DirectivasComponent } from './components/directivas/directivas.component';
+import { DirectivaPersonalizadaDirective } from './directives/directiva-personalizada.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectivasComponent
+    DirectivasComponent,
+    DirectivaPersonalizadaDirective
   ],
   imports: [
     BrowserModule,
