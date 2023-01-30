a3bee7f200a3cf109e797b88f8f428e44005703f
tati oliveira <tat1roliv@gmail.com>
2023-01-30 20:01:42 -0300

    generate components

---
+++ 06-forms-desafio/formsTOliveira/src/app/components/content-form/content-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-form',
+  templateUrl: './content-form.component.html',
+  styleUrls: ['./content-form.component.css']
+})
+export class ContentFormComponent {
+
+}
