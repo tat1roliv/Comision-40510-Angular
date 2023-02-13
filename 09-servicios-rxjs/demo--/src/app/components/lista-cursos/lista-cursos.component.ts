8ceebbb7a664a2a14d2181528bf4ef4b4a7310f8
tati oliveira <tat1roliv@gmail.com>
2023-02-13 19:35:46 -0300

    implemens ngOnInt

--- 09-servicios-rxjs/demo--/src/app/components/lista-cursos/lista-cursos.component.ts
+++ 09-servicios-rxjs/demo--/src/app/components/lista-cursos/lista-cursos.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
 import { Curso } from 'src/app/models/curso';
 import { CursoService } from 'src/app/services/curso.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lista-cursos',
   templateUrl: './lista-cursos.component.html',
   styleUrls: ['./lista-cursos.component.css']
 })
-export class ListaCursosComponent {
+export class ListaCursosComponent implements OnInit {
 
   cursos!: Curso[];
 
   constructor(
     private cursoService: CursoService
   ){
+    //this.cursos = cursoService.obtenerCursos();
     console.log("datos lista cursos", cursoService.obtenerCursos())
   }
- 
+
+  ngOnInit(): void {
+    this.cursos = this.cursoService.obtenerCursos();
+  }
+
 }
--- 09-servicios-rxjs/demo--/src/app/components/tabla-cursos/tabla-cursos.component.ts
+++ 09-servicios-rxjs/demo--/src/app/components/tabla-cursos/tabla-cursos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Curso } from 'src/app/models/curso';
 import { CursoService } from 'src/app/services/curso.service';
 import { MatTableDataSource } from '@angular/material/table';
@@ -8,18 +8,22 @@ import { MatTableDataSource } from '@angular/material/table';
   templateUrl: './tabla-cursos.component.html',
   styleUrls: ['./tabla-cursos.component.css']
 })
-export class TablaCursosComponent {
-  cursos!: Curso[];
+export class TablaCursosComponent implements OnInit {
+  //cursos!: Curso[];
 
-  dataSource: MatTableDataSource<Curso> ;
+  dataSource!: MatTableDataSource<Curso> ;
 
   columnas: string[] = ['nombre', 'comision', 'profesor', 'fechaInicio', 'fechaFin', 'inscripcionAbierta', 'acciones']
 
   constructor(
     private cursoService: CursoService
   ){
-    this.cursos = cursoService.obtenerCursos();
-    this.dataSource = new MatTableDataSource<Curso>(this.cursos);
+    //this.cursos = cursoService.obtenerCursos();
+    //this.dataSource = new MatTableDataSource<Curso>(this.cursos);
     //console.log("datos tabla cursos", cursoService.obtenerCursos())
+  }
+
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<Curso>(this.cursoService.obtenerCursos());
   }
 }
