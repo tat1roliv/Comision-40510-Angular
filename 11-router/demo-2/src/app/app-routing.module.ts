d99cbdf619f824e777141e1555099efb0b7577ba
tati oliveira <tat1roliv@gmail.com>
2023-02-19 13:55:30 -0300

    basic routing

--- 11-router/demo-2/src/app/app-routing.module.ts
+++ 11-router/demo-2/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { LoginComponent } from './components/login/login.component';
+import { PaginaNoEncontradaComponent } from './components/pagina-no-encontrada/pagina-no-encontrada.component';
 
-const routes: Routes = [];
+// localhost:4200/login -> login.component
+// localhost:4200/inicio -> inicio.component
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: '', redirectTo: 'inicio', pathMatch:'full'} ,
+  {path: '**', component: PaginaNoEncontradaComponent} ,
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- 11-router/demo-2/src/app/app.module.ts
+++ 11-router/demo-2/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { PaginaNoEncontradaComponent } from './components/pagina-no-encontrada/pagina-no-encontrada.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    PaginaNoEncontradaComponent,
   ],
   imports: [
     BrowserModule,
