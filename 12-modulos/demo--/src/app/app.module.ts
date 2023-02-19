8e307aba7dea3839ce1903568a4b4b49b65b7312
tati oliveira <tat1roliv@gmail.com>
2023-02-19 20:14:25 -0300

    update routes / module cursos

--- 12-modulos/demo--/src/app/app.module.ts
+++ 12-modulos/demo--/src/app/app.module.ts
@@ -4,31 +4,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ListaCursosComponent } from './components/lista-cursos/lista-cursos.component';
+
 import { TablaCursosComponent } from './components/tabla-cursos/tabla-cursos.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { CursoService } from './services/curso.service';
-import { CursoAlphaService } from './services/curso-alpha.service';
-import { cursos } from './services/cursos.data';
+//import { CursoService } from './services/curso.service';
+//import { cursos } from './services/cursos.data';
 
-import { env } from 'src/enviroment/enviroment';
+
 import { NoEncontradoComponent } from './components/no-encontrado/no-encontrado.component';
 import { InicioComponent } from './components/inicio/inicio.component';
-import { DetalleCursoComponent } from './components/detalle-curso/detalle-curso.component';
-import { AgregarCursoComponent } from './components/agregar-curso/agregar-curso.component';
-//import { config, token } from './config';
+import { CursosModule } from './cursos/cursos.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaCursosComponent,
     TablaCursosComponent,
     NoEncontradoComponent,
     InicioComponent,
-    DetalleCursoComponent,
-    AgregarCursoComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,7 +32,8 @@ import { AgregarCursoComponent } from './components/agregar-curso/agregar-curso.
     FormsModule,
     ReactiveFormsModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule 
   ],
   providers: [
   ],
--- 12-modulos/demo--/src/app/cursos/cursos.module.ts
+++ 12-modulos/demo--/src/app/cursos/cursos.module.ts
@@ -1,12 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AgregarCursoComponent } from './components/agregar-curso/agregar-curso.component';
+import { DetalleCursoComponent } from './components/detalle-curso/detalle-curso.component';
+import { ListaCursosComponent } from './components/lista-cursos/lista-cursos.component';
+import { EditarCursoComponent } from './components/editar-curso/editar-curso.component';
+import { MaterialModule } from '../material.module';
+import { CursosRountingModule } from './cursos-routing.module';
+//import { CursosService } from './services/cursos.service';
+//import { TablaCursosComponent } from './components/tabla-cursos/tabla-cursos.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AgregarCursoComponent,
+    DetalleCursoComponent,
+    ListaCursosComponent,
+    EditarCursoComponent,
+    //TablaCursosComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    CursosRountingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    CursosRountingModule
+  ],
+  providers: [
+    //CursosService
   ]
 })
 export class CursosModule { }
