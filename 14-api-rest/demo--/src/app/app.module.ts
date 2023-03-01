3638f2bd91d5f478676f59d65db89b56057e3349
tati oliveira <tat1roliv@gmail.com>
2023-03-01 10:09:38 -0300

    ++files

---
+++ 14-api-rest/demo--/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
+import { CursosModule } from './cursos/cursos.module';
+import { NoEncontradoComponent } from './core/components/no-encontrado/no-encontrado.component';
+import { InicioComponent } from './core/components/inicio/inicio.component';
+import { TablaCursosComponent } from './cursos/components/tabla-cursos/tabla-cursos.component';
+import { ReactiveFormsModule } from '@angular/forms';
+//import { CoreModule } from './core/components/core.module';
+//import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TablaCursosComponent,
+    NoEncontradoComponent,
+    InicioComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    //CursosModule,
+    AppRoutingModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 14-api-rest/demo--/src/app/cursos/components/lista-cursos/lista-cursos.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Curso } from 'src/app/models/curso';
+import { Sesion } from 'src/app/models/sesion';
+import { CursosService } from '../../services/cursos.service';
+import { SesionService } from '../../../core/services/sesion.service';
+
+
+@Component({
+  selector: 'app-lista-cursos',
+  templateUrl: './lista-cursos.component.html',
+  styleUrls: ['./lista-cursos.component.css']
+})
+
+export class ListaCursosComponent implements OnInit{
+  cursos!: Curso[];
+  cursos$!: Observable<Curso[]>;
+  sesion$!: Observable<Sesion>;
+
+  constructor(
+    private cursoService: CursosService,
+    private router: Router,
+    private sesion: SesionService
+  ){}
+
+  ngOnInit() {
+    this.cursos$ = this.cursoService.obtenerCursos();
+
+    this.sesion$ = this.sesion.obtenerSesion();
+    /*
+    this.sesion.obtenerSesion().subscribe( (sesion: Sesion) => {
+      console.log('estado de la sesion', sesion );
+      if(!sesion.sesionActiva){
+        this.router.navigate(['/auth/login']);
+      }
+    });
+    */
+  }
+
+  eliminarCurso(curso: Curso){
+    this.cursoService.eliminarCurso(curso);
+  }
+
+  redirigirEditarCurso(curso: Curso){
+    this.router.navigate(['cursos/editar', curso]);
+  }
+
+
+}
+
