0375927f6bf044ff22c71aa0b03304d2bfd0bb8d
tati oliveira <tat1roliv@gmail.com>
2023-03-14 19:43:56 -0300

    ng add @ngrx/store

--- 16-redux-ngrx/demo--/src/app/app.module.ts
+++ 16-redux-ngrx/demo--/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     SharedModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
